655a2c36e5143dac5a1320be5f54c373ab7a5409
karola15134 <karolina.sz1993@gmail.com>
2018-03-01 09:57:15 +0100

    clear button - event handling OnClick

--- app/src/main/java/com/mastermind/mastermind/activities/StatisticGameActivity.java
+++ app/src/main/java/com/mastermind/mastermind/activities/StatisticGameActivity.java
@@ -2,6 +2,8 @@ package com.mastermind.mastermind.activities;
 
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
@@ -9,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ListView;
 import android.widget.Spinner;
@@ -37,21 +40,29 @@ public class StatisticGameActivity extends AppCompatActivity implements
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_statisticlist);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_statisticlist);
 
-               final DBhandler db = new DBhandler(this);
-               final Spinner spinner = (Spinner)findViewById(R.id.sortSpinner);
+        final DBhandler db = new DBhandler(this);
+        final Spinner spinner = (Spinner) findViewById(R.id.sortSpinner);
 
-            spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+        for (int i = 0; i < 10; i++) {
+            db.addStat(new StatisticGame(new Date().toString(), GameVariantEnum.CLASSIC, 10, SolutionEnum.GENETIC_ALG));
+            db.addStat(new StatisticGame(new Date().toString(), GameVariantEnum.CLASSIC, 10, SolutionEnum.USER));
+        }
+
+
+        List<StatisticGame> itemsAll = db.getAllStat();
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 
             @Override
             public void onItemSelected(AdapterView<?> arg0, View arg1,
                                        int id, long position) {
 
                 List<StatisticGame> itemsAll = db.getAllStat();
-                switch((int)position)
-                {
+
+                switch ((int) position) {
                     case 0:
                         StatisticListViewAdapter adapter = new StatisticListViewAdapter(StatisticGameActivity.this,
                                 R.layout.statisticlistitem, itemsAll);
@@ -60,9 +71,8 @@ public class StatisticGameActivity extends AppCompatActivity implements
 
                     case 1:
                         List<StatisticGame> itemsUser = new ArrayList<StatisticGame>();
-                        for(StatisticGame game : itemsAll){
-                            if(game.getTypeOfSolution().equals(SolutionEnum.USER))
-                            {
+                        for (StatisticGame game : itemsAll) {
+                            if (game.getTypeOfSolution().equals(SolutionEnum.USER)) {
                                 itemsUser.add(game);
                             }
                         }
@@ -74,9 +84,8 @@ public class StatisticGameActivity extends AppCompatActivity implements
 
                     case 2:
                         List<StatisticGame> itemsGenetic = new ArrayList<StatisticGame>();
-                        for(StatisticGame game : itemsAll){
-                            if(game.getTypeOfSolution().equals(SolutionEnum.GENETIC_ALG))
-                            {
+                        for (StatisticGame game : itemsAll) {
+                            if (game.getTypeOfSolution().equals(SolutionEnum.GENETIC_ALG)) {
                                 itemsGenetic.add(game);
                             }
                         }
@@ -93,26 +102,63 @@ public class StatisticGameActivity extends AppCompatActivity implements
 
             @Override
             public void onNothingSelected(AdapterView<?> arg0) {
-                // ta metoda wykonuje sie gdy lista zostanie wybrana, ale nie zostanie wybrany żaden element z listy
+
 
             }
         });
 
 
-                menu = (ListView) findViewById(R.id.statisticMenuListView);
+        menu = (ListView) findViewById(R.id.statisticMenuListView);
 
 
-                StatisticListViewAdapter adapter = new StatisticListViewAdapter(this,
-                        R.layout.statisticlistitem, items);
-                menu.setAdapter(adapter);
-
+        StatisticListViewAdapter adapter = new StatisticListViewAdapter(this,
+                R.layout.statisticlistitem, items);
+        menu.setAdapter(adapter);
 
 
     }
 
 
-            @Override
-            public void onItemClick (AdapterView < ? > adapterView, View view,int i, long l){
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
+    }
+
+    public void onButtonClick(View view) {
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage("Jesteś pewien? \nPotwierdzenie oznacza usunięcie wszystkich statystyk");
+
+        alertDialogBuilder.setPositiveButton("Tak", new DialogInterface.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onClick(DialogInterface arg0, int arg1) {
+                DBhandler db = new DBhandler(StatisticGameActivity.this);
+                List<StatisticGame> list = db.getAllStat();
+
+                for (StatisticGame game : list) {
+                    db.deleteStat(game);
+                }
+
+                list=db.getAllStat();
+                Toast.makeText(StatisticGameActivity.this, "Usunieto", Toast.LENGTH_LONG).show();
+
+                menu = (ListView) findViewById(R.id.statisticMenuListView);
+                StatisticListViewAdapter adapterE = new StatisticListViewAdapter(StatisticGameActivity.this,
+                        R.layout.statisticlistitem, list);
+                menu.setAdapter(adapterE);
             }
-        }
+        });
+
+        alertDialogBuilder.setNegativeButton("Nie", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
+    }
