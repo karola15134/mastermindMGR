e53cdc1c14eef85a19592a2b9e1d1cc10033007e
karola15134 <karolina.sz1993@gmail.com>
2018-03-19 16:16:41 +0100

    buttons with colors

--- app/src/main/java/com/mastermind/mastermind/activities/game/MainGameActivity.java
+++ app/src/main/java/com/mastermind/mastermind/activities/game/MainGameActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.mastermind.mastermind.R;
 import com.mastermind.mastermind.bean.game.ColorList;
@@ -15,7 +16,9 @@ import com.mastermind.mastermind.bean.game.MainGame;
 import com.mastermind.mastermind.enums.ColorEnum;
 import com.mastermind.mastermind.enums.GameVariantEnum;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainGameActivity extends AppCompatActivity {
 
@@ -39,15 +42,54 @@ public class MainGameActivity extends AppCompatActivity {
         ColorList color  = new ColorList(variantGame);  //random colors
         ColorMap colorMap = new ColorMap(); //add real color to ColorEnum
 
+        fillButtons(colorMap);
+
+
+
+        if(variantGame.equals(GameVariantEnum.CLASSIC))  {
+            LinearLayout layout = (LinearLayout) findViewById(R.id.superLayout);
+            layout.setVisibility(LinearLayout.GONE);
+
+        }
+
 
         List<ColorEnum> colorsRand = color.getColorsRand();
 
-        MainGame game = new MainGame();
-        game.runGame(colorsRand);
 
 
 
 
+
+
+    }
+
+    private void fillButtons(ColorMap colorMap) {
+
+        Map<ColorEnum,Integer> colorsMap = colorMap.getColorsMap();
+
+        Button button1 = (Button) findViewById(R.id.color1);
+        button1.setBackgroundColor(colorsMap.get(ColorEnum.GREEN));
+
+        button1=(Button) findViewById(R.id.color2);
+        button1.setBackgroundColor(colorsMap.get(ColorEnum.RED));
+
+        button1=(Button) findViewById(R.id.color3);
+        button1.setBackgroundColor(colorsMap.get(ColorEnum.YELLOW));
+
+        button1=(Button) findViewById(R.id.color4);
+        button1.setBackgroundColor(colorsMap.get(ColorEnum.LIGTH_GREY));
+
+        button1=(Button) findViewById(R.id.color5);
+        button1.setBackgroundColor(colorsMap.get(ColorEnum.DARK_GREY));
+
+        button1=(Button) findViewById(R.id.color6);
+        button1.setBackgroundColor(colorsMap.get(ColorEnum.BLUE));
+
+        button1=(Button) findViewById(R.id.color7);
+        button1.setBackgroundColor(colorsMap.get(ColorEnum.ORANGE));
+
+        button1=(Button) findViewById(R.id.color8);
+        button1.setBackgroundColor(colorsMap.get(ColorEnum.DARK_BLUE));
 
     }
 
